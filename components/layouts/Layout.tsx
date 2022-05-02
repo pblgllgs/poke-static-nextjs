dfb6556d0e7c907002f9dc4caf5d94878bebf870
pblgllgs <pbl.gllgs@gmail.com>
2022-05-02 09:10:41 -0400

    final

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -7,7 +7,10 @@ interface LayoutProps {
   children: React.ReactNode;
 }
 
+const origin = (typeof window === 'undefined') ? '' : window.location.origin;
+
 export const Layout: FC<LayoutProps> = ({ children, title }) => {
+
   return (
     <>
       <Head>
@@ -16,6 +19,18 @@ export const Layout: FC<LayoutProps> = ({ children, title }) => {
         <meta name="author" content="pblgllgs" />
         <meta name="description" content={`info ${title}`} />
         <meta name="keywords" content={`${title}, pokemon, pokedex`} />
+        <meta
+          property="og:title"
+          content={`Información sobre ${title}`}
+        />
+        <meta
+          property="og:description"
+          content={`Esta es la página sobre ${title}`}
+        />
+        <meta
+          property="og:image"
+          content={`${origin}/img/banner.png`}
+        />
       </Head>
 
       <Navbar />
